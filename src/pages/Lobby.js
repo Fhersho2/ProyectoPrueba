070479426a7ccfecdddacb1697c53bc270a394e4
FernandoJOP <fherosoria@gmail.com>
2021-04-05 18:11:40 -0500

    Avance en vista de Usuarios

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,30 +1,44 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "./styles/Lobby.css";
+import Fondo from "../images/astronaut.jpg";
 
 function Lobby(props) {
+  const name = "Alumnos";
   return (
     <div className="container_fluid">
       <header>
         <Navbar />
         <div className="principal-section">
-          <form>
-            <div className="form-group">
-              <label>Usuario</label>
-              <input
-                className="form-control"
-                type="text"
-                name="user"
-              />
+          <img src={Fondo} className="principal-image" alt="Estudiantes"></img>
+          <div className="principal-layer">
+            <div className="layer-item">
+              <h3>Bienvenido!!!!</h3>
+              <p>
+                La plataorma Arkosred
+              </p>
             </div>
-            <div className="form-group">
-              <label>Contraseña</label>
-              <input
-                className="form-control"
-                type="password"
-                name="password"
-              />
-            </div>
-          </form>
+            <form className="sesion-form">
+              <div className="form_title">
+                <h5>Plataforma {name}</h5>
+              </div>
+              <div className="form-group">
+                <label>Usuario</label>
+                <input className="form-control" type="text" name="user" />
+              </div>
+              <div className="form-group">
+                <label>Contraseña</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                />
+              </div>
+              <button  className="btn btn-primary">
+                Inciar Sesion
+              </button>
+            </form>
+          </div>
         </div>
       </header>
     </div>

72180fcb9504631c59aafac82a7225adfdc71369
Temo Castro <temocastro9@gmail.com>
2021-04-23 23:29:15 -0500

    validamos inicio de sesion

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,19 @@
+// Dependencies
+import React from 'react'
+
+// Components
+
+// Styles
+//import './style.css'
+
+const Dashboard = () => {
+
+  return (
+    <div className=''>
+        <h1>bienvenido al dashboard</h1>
+    </div>
+  )
+}
+
+
+export default Dashboard
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,9 +3,30 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./styles/Lobby.css";
 import Fondo from "../images/astronaut.jpg";
+import { useState, useEffect } from "react";
+import Axios from 'axios';
 
 function Lobby(props) {
   const name = "Alumnos";
+  const [userName, setUserName] = useState();
+  const [userPass, setUserpass] = useState();
+  const [loginStatus, setLoginStatus] = useState();
+
+  const login = () => {
+    Axios.post("http://localhost:3001/api/auth", {
+      username: userName,
+      password: userPass,
+    }).then((response) => {
+      console.log(response.data)
+    });
+  };
+
+  
+
+  useEffect(() =>{
+    
+   }, [])
+  
   return (
     <div className="container_fluid">
       <header>
@@ -15,9 +36,7 @@ function Lobby(props) {
           <div className="principal-layer">
             <div className="layer-item">
               <h3>Bienvenido!!!!</h3>
-              <p>
-                La plataorma Arkosred
-              </p>
+              <p>La plataorma Arkosred</p>
             </div>
             <form className="sesion-form">
               <div className="form_title">
@@ -25,20 +44,28 @@ function Lobby(props) {
               </div>
               <div className="form-group">
                 <label>Usuario</label>
-                <input className="form-control" type="text" name="user" />
+                <input 
+                  className="form-control" 
+                  type="text" 
+                  onChange={(e) =>{
+                    setUserName(e.target.value);
+                  }} />
               </div>
               <div className="form-group">
                 <label>Contraseña</label>
                 <input
                   className="form-control"
                   type="password"
-                  name="password"
+                  onChange={ (e) =>{
+                    setUserpass(e.target.value)
+                  }}
                 />
               </div>
               <div className="form-btn">
-                <button  className="btn btn-primary btn-sm">
-                  Iniciar Sesion
-                </button>
+                <button className="btn btn-primary btn-sm"
+                  type="button"
+                  onClick={login}
+                >Iniciar Sesion</button>
               </div>
             </form>
           </div>
